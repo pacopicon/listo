30a25726923a1fb613fe84c9a84d2869eac29e15
Paco <palmtreerooskee@gmail.com>
2017-01-01 17:15:41 -0500

    code cleanup

--- app/scripts/controllers/AuthCtrl.js
+++ app/scripts/controllers/AuthCtrl.js
@@ -7,70 +7,7 @@ listo.controller('AuthCtrl', ["$scope", "ItemCrud", "$rootScope", "$firebaseAuth
 
     var auth = $firebaseAuth(ref);
 
-    // returns promise
-    auth.signInWithEmailAndPassword(email, pass);
-    // returns promise
-    auth.createUserWtihEmailAndPassword(email, pass);
 
-    auth.onAuthStateChanged(firebaseUser );
-
-
-
-
-
-
-
-
-
-        //
-        //
-        // authObj.$createUser({
-        //   email: "my@email.com",
-        //   password: "mypassword"
-        // }).then(function(userData) {
-        //   console.log("User " + userData.uid + " created successfully!");
-        //
-        //   return authObj.$authWithPassword({
-        //     email: "my@email.com",
-        //     password: "mypassword"
-        //   });
-        // }).then(function(authData) {
-        //   console.log("Logged in as:", authData.uid);
-        // }).catch(function(error) {
-        //   console.error("Error: ", error);
-        // });
-        //
-        //
-        //
-        // authObj.$authWithPassword({
-        //   email: "my@email.com",
-        //   password: "mypassword"
-        // }).then(function(authData) {
-        //   console.log("Logged in as:", authData.uid);
-        // }).catch(function(error) {
-        //   console.error("Authentication failed:", error);
-        // });
-        //
-        //
-        //
-        // authObj.$onAuth(function(authData) {
-        //   if (authData) {
-        //     console.log("Logged in as:", authData.uid);
-        //   } else {
-        //     console.log("Logged out");
-        //   }
-        // });
-        //
-        //
-        //
-        // var authData = authObj.$getAuth();
-        //
-        // if (authData) {
-        //   console.log("Logged in as:", authData.uid);
-        // } else {
-        //   console.log("Logged out");
-        // }
-        //
 
 
 
