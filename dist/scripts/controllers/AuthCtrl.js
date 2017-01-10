a059e877e34cff8fadc73eb045797490aba3c335
Paco <palmtreerooskee@gmail.com>
2017-01-10 05:43:37 -0500

    creating dummy authentication for hosting

--- dist/scripts/controllers/AuthCtrl.js
+++ dist/scripts/controllers/AuthCtrl.js
@@ -1,5 +1,5 @@
-listo.controller('AuthCtrl', ["$scope", "ItemCrud", "UserCrud", "$rootScope", "$interval", "$log", "$http", "$locale", "$location", "$templateCache", "$timeout", "$q", "$sce", "$firebaseAuth",
-  function($scope, ItemCrud, UserCrud, $rootScope, $interval, $log, $http, $locale, $location, $templateCache, $timeout, $q, $sce, $firebaseAuth) {
+listo.controller('AuthCtrl', ["$scope", "$state", "ItemCrud", "UserCrud", "$rootScope", "$interval", "$log", "$http", "$locale", "$location", "$templateCache", "$timeout", "$q", "$sce", "$firebaseAuth",
+  function($scope, $state, ItemCrud, UserCrud, $rootScope, $interval, $log, $http, $locale, $location, $templateCache, $timeout, $q, $sce, $firebaseAuth) {
 
     // var ref = ItemCrud.getRef;
 
@@ -7,27 +7,43 @@ listo.controller('AuthCtrl', ["$scope", "ItemCrud", "UserCrud", "$rootScope", "$
     var auth = $firebaseAuth();
 
     // var user = $firebaseArray(ref);
- 
-    // sign in
 
-
-    // promise.catch(e => console.log(e.message));
+    var changeState = function('state') {
+      $state.go('state')
+    };
 
     $scope.login = function(email, pass) {
-      auth.signInWithEmailAndPassword(email, pass).then(function (authData) {
-        console.log(AuthData);
-      }, function(error) {
-        if (error){
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          if (errorCode === 'auth/wrong-password') {
-            alert('Wrong password.');
-          } else {
-            alert(errorMessage);
-          }
-        }
-      });
+      if (email === 'guest@guest.com' && pass === "palmtreerooskee@gmail.com") {
+        changeState('/userIncompleteItems');
+      } else {
+        $window.alert.("wrong e-mail and password");
+      }
     };
+
+
+
+
+
+    // $scope.login = function(email, pass) {
+    //   auth.signInWithEmailAndPassword(email, pass).then(function (authData) {
+    //     console.log(AuthData);
+    //   }, function(error) {
+    //     if (error){
+    //       var errorCode = error.code;
+    //       var errorMessage = error.message;
+    //       if (errorCode === 'auth/wrong-password') {
+    //         alert('Wrong password.');
+    //       } else {
+    //         alert(errorMessage);
+    //       }
+    //     }
+    //   });
+    // };
+
+
+
+
+
 
   }
 ]);
