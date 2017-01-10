3c7e8bba801495621ba2069d731bffe2d4060b27
Paco <palmtreerooskee@gmail.com>
2017-01-09 21:07:59 -0500

    working on authentication

--- app/scripts/controllers/AuthCtrl.js
+++ app/scripts/controllers/AuthCtrl.js
@@ -1,19 +1,19 @@
 listo.controller('AuthCtrl', ["$scope", "ItemCrud", "UserCrud", "$rootScope", "$interval", "$log", "$http", "$locale", "$location", "$templateCache", "$timeout", "$q", "$sce", "$firebaseAuth",
   function($scope, ItemCrud, UserCrud, $rootScope, $interval, $log, $http, $locale, $location, $templateCache, $timeout, $q, $sce, $firebaseAuth) {
 
-    var ref = ItemCrud.getRef;
+    // var ref = ItemCrud.getRef;
 
-    var auth = $firebaseAuth(ref);
+    // var a = $firebase.auth();
+    var auth = $firebaseAuth();
 
     // var user = $firebaseArray(ref);
 
     // sign in
 
 
-    promise.catch(e => console.log(e.message));
+    // promise.catch(e => console.log(e.message));
 
     $scope.login = function(email, pass) {
-      console.log("login called")
       auth.signInWithEmailAndPassword(email, pass).then(function (authData) {
         console.log(AuthData);
       }, function(error) {
